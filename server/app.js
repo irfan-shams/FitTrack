2132f3fa48af4f0e970550090f2a9fae6a78207a
irfan.shams <shams.irfan@gmail.com>
2026-07-09 16:59:06 +0500

    Setup authentication infrastructure

--- server/app.js
+++ server/app.js
@@ -2,21 +2,26 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import errorHandler from "./middleware/error.middleware.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+app.use("/api/auth", authRoutes);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/health", (req, res) => {
-  res.status(200).json({
+  res.json({
     success: true,
     message: "FitTrack API is running successfully."
   });
 });
+
+app.use(errorHandler);
 
 export default app;
